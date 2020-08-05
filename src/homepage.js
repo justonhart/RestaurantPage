61d57477c17a3f65ff97c43b5b644899c7912a9f
justonhart <contact@justonh.art>
2020-08-05 15:29:56 -0500

    Add script to create homepage

---
+++ src/homepage.js
@@ -0,0 +1,26 @@
+const renderHomepage = (() => {
+    const content = document.getElementById("content");
+
+    while(content.lastChild){
+        content.lastChild.remove();
+    }
+
+    let homebox = document.createElement('div');
+    homebox.id = "homebox";
+    content.appendChild(homebox);
+
+
+    let header = document.createElement('h1');
+    header.innerText = "Wonderful Restaurant";
+    homebox.appendChild(header);
+
+    let review = document.createElement('p');
+    review.innerText = `"Wonderful Restaurant is the best restaurant I've been to in the state! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias sit earum ducimus odio necessitatibus architecto. Est exercitationem, enim consectetur totam sapiente adipisci voluptatibus, sed eligendi reprehenderit labore illum! Dignissimos, deserunt?" ~ Plato`;
+    homebox.appendChild(review);
+
+    let image = document.createElement('img');
+    image.setAttribute('src', '../table.jpg');
+    homebox.appendChild(image);
+});
+
+export {renderHomepage};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('logged');
+import {renderHomepage} from "./homepage";
+import {renderMenuPage} from "./menupage";
+import {renderContactPage} from "./contactpage";
+
+document.getElementById("homeButton").addEventListener("click", renderHomepage);
+document.getElementById("menuButton").addEventListener("click", renderMenuPage);
+document.getElementById("contactButton").addEventListener("click", renderContactPage);
+
+renderHomepage();
