e6dc91ac8196412eb13e386340c4f00ea7f07cb4
justonhart <contact@justonh.art>
2020-08-06 11:38:47 -0500

    Finish contact page

--- src/contactpage.js
+++ src/contactpage.js
@@ -3,6 +3,12 @@ const renderContactPage = (() => {
     while(content.lastChild){
         content.lastChild.remove();
     }
+
+    let contactContainer = document.createElement("div");
+    contactContainer.setAttribute("id", "contactContainer");
+    contactContainer.innerHTML = `<p>Name: Regular Business Owner</p><p>Phone: 123-456-7890</p><p>Email: fakebusinessaccount@email.provider</p>`;
+
+    content.appendChild(contactContainer);
 });
 
 export {renderContactPage};
